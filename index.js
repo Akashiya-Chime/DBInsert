60f8cf4225e498f6c2b6929d708a2aa77160c25e
Akashiya <akashiyachime@163.com>
2020-07-10 12:15:19 +0800

    Yes, successfully finish sending data!

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const app = express()
+const bodyParser = require('body-parser')
 
 // 配置 body-parser
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+//设置跨域请求
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+  });
+
 app.engine('html', require('express-art-template'))
 
 app.use('/src', express.static('./views/src'))
 
-app.get('/', (req, res) => {
+app.get('/',(req, res) =>{
     res.render('index.html')
 })
 
 app.post('/post', (req, res) => {
     res.send(req.body)
 })
-
+ 
 app.listen(80, () => {
     console.log('Serve is on...')
 })
